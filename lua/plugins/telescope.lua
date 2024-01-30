49ddcc280539c995841b7a18e164736fb0ced7ff
Chandrasekhar Thumuluru <chandra.thumuluru+github@gmail.com>
2024-01-29 20:20:39 -0800

    Change telescope theme to dropdown for pickers find_files, live_grep and buffers.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,25 @@
+-- TODO(chandrat) add LSP picker later
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    -- Required dependencies
-    "nvim-lua/popup.nvim",
-    "nvim-lua/plenary.nvim",
-    -- Suggested dependencies
-    "BurntSushi/ripgrep",
-    "nvim-telescope/telescope-fzf-native.nvim",
-    -- Optional dependencies
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
-  },
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        -- Required dependencies
+        "nvim-lua/popup.nvim",
+        "nvim-lua/plenary.nvim",
+        -- Suggested dependencies
+        "BurntSushi/ripgrep",
+        "nvim-telescope/telescope-fzf-native.nvim",
+        -- Optional dependencies
+        "sharkdp/fd",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("telescope").setup {
+            pickers = {
+                find_files = { theme = "dropdown" },
+                live_grep = { theme = "dropdown" },
+                buffers = { theme = "dropdown" },
+            },
+        }
+    end,
 }
